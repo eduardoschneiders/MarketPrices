a7f6dfb908b1964721a5de14287b46cbadc27126
Eduardo Matheus Schneiders <eduardo.schneiders@cwi.com.br>
2019-04-23 15:36:45 -0300

    List markets

--- app/src/main/java/com/eduardo/marketprices/MainActivity.java
+++ app/src/main/java/com/eduardo/marketprices/MainActivity.java
@@ -3,10 +3,14 @@ package com.eduardo.marketprices;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.eduardo.marketprices.Models.Market;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         final Button createMarket = (Button) findViewById(R.id.createMarket);
         final EditText marketName = (EditText) findViewById(R.id.marketName);
 
+        updateList();
+
         createMarket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,11 +33,28 @@ public class MainActivity extends AppCompatActivity {
                 if (!name.equals("")){
                     Market market = new Market(name);
                     market.save(getApplicationContext());
+
+                    updateList();
+                    marketName.setText("");
+
                     Util.popUp(getApplicationContext(), "Market " + name + " created with success.");
                 } else {
                     Util.popUp(getApplicationContext(), "Error to create market " + name + "!");
                 }
             }
         });
+    }
+
+    private void updateList(){
+        ArrayList<Market> markets = Market.all(getApplicationContext());
+        ArrayList<String> results = new ArrayList<>();
+
+        for (Market market : markets){
+            results.add(market.getId() + " => " + market.getName());
+        }
+
+        ListView mainListView = (ListView) findViewById(R.id.mainListView);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.row_list_view, results);
+        mainListView.setAdapter(listAdapter);
     }
 }
--- app/src/main/java/com/eduardo/marketprices/Models/Market.java
+++ app/src/main/java/com/eduardo/marketprices/Models/Market.java
@@ -2,14 +2,32 @@ package com.eduardo.marketprices.Models;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 
 import com.eduardo.marketprices.DataSource.DataSource;
 
-public class Market {
-    String name;
+import java.util.ArrayList;
 
-    public Market(String name){
+public class Market {
+    private static String TABLE_NAME = "markets";
+    String name;
+    Integer id;
+
+    public Market(String name) {
         this.name = name;
+    }
+
+    public Market(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
     }
 
     public void save(Context ctx) {
@@ -17,6 +35,30 @@ public class Market {
         values.put("name", this.name);
 
         DataSource ds = new DataSource(ctx);
-        ds.save(values, "markets");
+        ds.save(values, TABLE_NAME);
+    }
+
+    public static ArrayList<Market> all(Context ctx){
+        DataSource ds = new DataSource(ctx);
+        Cursor cursor = ds.search(TABLE_NAME, null, null, null, null, null, null, null);
+        ArrayList<Market> results = new ArrayList<>();
+
+        if (cursor.getCount() > 0 ){
+            cursor.moveToFirst();
+            for (int i = 0; i < cursor.getCount(); i++){
+                Market market = Market.buildFromDatabase(cursor);
+                results.add(market);
+                cursor.moveToNext();
+            }
+        }
+
+        return results;
+    }
+
+    private static Market buildFromDatabase(Cursor cursor){
+        Integer id = cursor.getInt(cursor.getColumnIndex("id"));
+        String name = cursor.getString(cursor.getColumnIndex("name"));
+        Market market = new Market(id, name);
+        return market;
     }
 }
